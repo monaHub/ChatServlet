0cc7f179758bbe95b5c4102b14b8f1b500e0e2c3
Mona <m_arjang@yahoo.com>
2017-03-26 04:15:31 -0600

    add fieldset and legend, better interface!

--- LogInChat.java
+++ LogInChat.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/LogIn")
 public class LogInChat extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; 
        
     /**
      * @see HttpServlet#HttpServlet()
